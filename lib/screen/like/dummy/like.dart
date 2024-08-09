cd5d71f9cc0fccebe10ca08931258260137077cd
lhyeonnit <lhyeonnit@gmail.com>
2024-08-09 11:08:47 +0900

    like

---
+++ lib/screen/like/dummy/like.dart
@@ -0,0 +1,50 @@
+// dummy_data.dart
+
+final List<String> categories = [
+  '전체',
+  '아우터',
+  '상의',
+  '하의',
+  '슈즈',
+  '세트/한벌옷',
+  '언더웨어/홈웨어',
+];
+
+final List<Map<String, String>> items = [
+  {
+    'image': 'http://example.com/item1.png',
+    'name': '꿈꾸는데이지 안나 토션 레이스 베스트',
+    'brand': '꿈꾸는데이지',
+    'price': '32,800원',
+    'discount': '15%',
+    'likes': '13,000',
+    'comments': '49'
+  },
+  {
+    'image': 'http://example.com/item2.png',
+    'name': '레인보우꿈 안나 토션 레이스 베스트',
+    'brand': '레인보우꿈',
+    'price': '32,800원',
+    'discount': '15%',
+    'likes': '13,000',
+    'comments': '49'
+  },
+  {
+    'image': 'http://example.com/item3.png',
+    'name': '기글옷장 안나 토션 레이스 베스트',
+    'brand': '기글옷장',
+    'price': '32,800원',
+    'discount': '15%',
+    'likes': '13,000',
+    'comments': '49'
+  },
+  {
+    'image': 'http://example.com/item4.png',
+    'name': '스파클나라 안나 토션 레이스 베스트',
+    'brand': '스파클나라',
+    'price': '32,800원',
+    'discount': '15%',
+    'likes': '13,000',
+    'comments': '49'
+  },
+];
